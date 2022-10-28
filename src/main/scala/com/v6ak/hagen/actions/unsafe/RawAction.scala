fd611797b6871d902035e732a98e18cb281461fe
Vít Šesták <v6ak@users.noreply.github.com>
2022-10-28 15:11:29 +0200

    Allow Elements in RawAction

--- src/main/scala/com/v6ak/hagen/actions/unsafe/RawAction.scala
+++ src/main/scala/com/v6ak/hagen/actions/unsafe/RawAction.scala
@@ -2,7 +2,8 @@ package com.v6ak.hagen.actions.unsafe
 
 import com.v6ak.hagen.actions.Action
 import com.v6ak.hagen.expressions.{Context, Entity}
+import com.v6ak.hagen.materializeElements
 
-final case class RawAction(structure: Any, variables: Set[Entity[_]]) extends Action {
-  override def toStructure(context: Context): Any = structure
+final case class RawAction(structure: Map[String, _], variables: Set[Entity[_]]) extends Action {
+  override def toStructure(context: Context): Any = materializeElements(context, structure)
 }
