6e6840bbb0bd02cedba5bbf798c0c07fc8905777
Vít Šesták <v6ak@users.noreply.github.com>
2022-10-28 15:24:49 +0200

    Expr implements Element

--- src/main/scala/com/v6ak/hagen/expressions/Expr.scala
+++ src/main/scala/com/v6ak/hagen/expressions/Expr.scala
@@ -1,7 +1,8 @@
 package com.v6ak.hagen.expressions
+import com.v6ak.hagen.Element
 import com.v6ak.hagen.expressions.BooleanOps.*
 
-trait Expr[T]:
+trait Expr[T] extends Element:
 
   def asJinja(context: Context): String
 
@@ -16,6 +17,10 @@ trait Expr[T]:
   def transform(transformer: Transformer): Expr[T]
 
   def variables: Set[Entity[_]]
+
+  override def toStructure(context: Context): Any = asCompleteJinja(context)
+
+  override def defined: Set[Entity[_]] = Set()
 
   def matched[O](options: (Expr[T], Expr[O])*)(defaultValue: Expr[O]): Expr[O] = {
     Assign(this)(entityValue =>
