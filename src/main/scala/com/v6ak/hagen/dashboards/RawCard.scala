14308cecfe66453da89fc1efdc21d59b968fb0de
Vít Šesták <v6ak@users.noreply.github.com>
2022-10-28 15:13:01 +0200

    Allow Elements in RawCard

--- src/main/scala/com/v6ak/hagen/dashboards/RawCard.scala
+++ src/main/scala/com/v6ak/hagen/dashboards/RawCard.scala
@@ -1,7 +1,8 @@
 package com.v6ak.hagen.dashboards
 
+import com.v6ak.hagen.materializeElements
 import com.v6ak.hagen.dashboards.Card
 import com.v6ak.hagen.expressions.{Context, Entity}
 
 case class RawCard(structure: Map[String, Any], override val variables: Set[Entity[_]]) extends Card:
-  override def toStructure(context: Context): Any = structure
+  override def toStructure(context: Context): Any = materializeElements(context, structure)
