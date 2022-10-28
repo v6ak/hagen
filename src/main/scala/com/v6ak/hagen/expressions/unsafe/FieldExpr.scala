e45370eba0f43645ef9ff4462867a528a9f2d8dc
Vít Šesták <v6ak@users.noreply.github.com>
2022-10-28 15:14:40 +0200

    Add support for using fields in Jinja

---
+++ src/main/scala/com/v6ak/hagen/expressions/unsafe/FieldExpr.scala
@@ -0,0 +1,16 @@
+package com.v6ak.hagen.expressions.unsafe
+
+import com.v6ak.hagen.expressions.{Context, Entity, Expr, Transformer}
+
+class FieldExpr[I, O](a: Expr[I], field: String) extends Expr[O]:
+  override def asJinja(context: Context): String = s"${a.asContextSafeJinja(context)}.$field"
+
+  override def transform(transformer: Transformer): Expr[O] = transformer.transformBottomUp(
+    FieldExpr(
+      transformer.transformTopDown(a).transform(transformer),
+      field,
+    )
+  )
+
+  override def variables: Set[Entity[_]] = a.variables
+
