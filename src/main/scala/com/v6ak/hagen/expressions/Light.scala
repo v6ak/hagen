3c08de0a2853effb9943c1dad3490696e46414b0
Vít Šesták <v6ak@users.noreply.github.com>
2022-10-28 15:25:35 +0200

    Simplify Light

--- src/main/scala/com/v6ak/hagen/expressions/Light.scala
+++ src/main/scala/com/v6ak/hagen/expressions/Light.scala
@@ -13,9 +13,9 @@ final case class Light(name: String) extends Entity[Boolean] with SwitchableEnti
     transition: Option[Expr[Double]] = None,
   ): Action = ServiceCall(
     s"$entityType.turn_on", this, data =
-      optionalMap("color_temp", colorTemp.map(value => value.asCompleteJinja(Context.TemporaryHack))) ++
-        optionalMap("brightness_pct", brightnessPct.map(value => value.asCompleteJinja(Context.TemporaryHack))) ++
-        optionalMap("transition", transition.map(value => value.asCompleteJinja(Context.TemporaryHack)))
+      optionalMap("color_temp", colorTemp) ++
+        optionalMap("brightness_pct", brightnessPct) ++
+        optionalMap("transition", transition)
   )
 
   def decreateBrightness() = DecreaseBrightness(this)
