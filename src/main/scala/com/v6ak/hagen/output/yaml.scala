9b4cc447189326b74473a5f20c39a2a631ce111f
Vít Šesták <v6ak@users.noreply.github.com>
2022-10-29 12:39:19 +0200

    Add support for !include and !input

--- src/main/scala/com/v6ak/hagen/output/yaml.scala
+++ src/main/scala/com/v6ak/hagen/output/yaml.scala
@@ -2,10 +2,14 @@ package com.v6ak.hagen.output
 
 import org.snakeyaml.engine.v2.api.{Dump, DumpSettings, RepresentToNode}
 import org.snakeyaml.engine.v2.common.ScalarStyle
-import org.snakeyaml.engine.v2.nodes.Node
+import org.snakeyaml.engine.v2.nodes.{Node, ScalarNode, Tag}
 import org.snakeyaml.engine.v2.representer.StandardRepresenter
 
 import scala.reflect.ClassTag
+
+case class Input(id: String)
+
+case class Include(id: String)
 
 object Yaml:
   def serialize(data: Any): String = {
@@ -29,7 +33,15 @@ final class ScalaStandardRepresenter(settings: DumpSettings) extends StandardRep
     }
     parentClassRepresenters.put(fromTag.runtimeClass, MappedRepresenter(nextRepresenter)(f))
   }
+  def addScalarTagMapping[T](tag: Tag, f: T => String)(implicit classTag: ClassTag[T]): Unit = {
+    representers.put(classTag.runtimeClass, obj => {
+      new ScalarNode(tag, f(obj.asInstanceOf[T]), ScalarStyle.SINGLE_QUOTED)
+    })
+  }
+
   import scala.jdk.CollectionConverters.*
   addMapping[Map[_, _], java.util.Map[_, _]](_.asJava)
   addMapping[Seq[_], java.util.List[_]](_.asJava)
   addMapping[Set[_], java.util.Set[_]](_.asJava)
+  addScalarTagMapping[Input](new Tag("!input"), _.id)
+  addScalarTagMapping[Include](new Tag("!include"), _.id)
