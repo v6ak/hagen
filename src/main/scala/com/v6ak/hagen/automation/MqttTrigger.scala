29aa83700a35425c5fa1bd0debd21c48b8478981
Vít Šesták <v6ak@users.noreply.github.com>
2022-10-29 11:19:36 +0200

    Renamed Zigbee2MqttTrigger to MqttTrigger (as it wasn't directly related to zigbee2mqtt) + added a helper method for zigbee2mqtt

--- src/main/scala/com/v6ak/hagen/automation/MqttTrigger.scala
+++ src/main/scala/com/v6ak/hagen/automation/MqttTrigger.scala
@@ -2,10 +2,13 @@ package com.v6ak.hagen.automation
 
 import com.v6ak.hagen.expressions.{Context, Entity}
 
-final case class Zigbee2MqttTrigger(topic: String) extends Trigger:
+final case class MqttTrigger(topic: String) extends Trigger:
   override def variables: Set[Entity[_]] = Set()
 
   override def toStructure(context: Context): Any = Map(
     "platform" -> "mqtt",
     "topic" -> topic,
   )
+
+object MqttTrigger:
+  def forZigbee2mqtt(id: String): MqttTrigger = MqttTrigger(s"zigbee2mqtt/$id")
