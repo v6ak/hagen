7a83dc8af5a945f7d5c1cee79d184994dec2fb25
Vít Šesták <v6ak@users.noreply.github.com>
2022-11-25 19:57:30 +0100

    Simplify BatteryPowered

--- src/main/scala/com/v6ak/hagen/extensions/batteryPowered/BatteryPowered.scala
+++ src/main/scala/com/v6ak/hagen/extensions/batteryPowered/BatteryPowered.scala
@@ -2,17 +2,8 @@ package com.v6ak.hagen.extensions.batteryPowered
 
 import com.v6ak.hagen.Element
 import com.v6ak.hagen.expressions.{Context, Entity}
+import com.v6ak.hagen.expressions.DoubleType
 
-final case class BatteryPowered(id: String, name: String, deviceType: String, criticalBattery: Double) extends Element {
-  override def toStructure(context: Context): Any = Map(
-    "entity_id" -> id,
-    "id" -> id,
-    "type" -> deviceType,
-    "name" -> name,
-    "low_battery_threshold" -> criticalBattery,
-  )
+final case class BatteryPowered(id: String, name: String, deviceType: String, criticalBattery: Double):
 
-  override def variables: Set[Entity[_]] = Set() // TODO
-
-  override def defined: Set[Entity[_]] = Set()
-}
+  def percentage: Entity[Double] = Entity[Double](s"sensor.${id}_battery")
