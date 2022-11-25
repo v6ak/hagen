07be308e76da2723d6e3194417fe473db3d6a2db
Vít Šesták <v6ak@users.noreply.github.com>
2022-11-25 19:55:55 +0100

    Glance: add more options

--- src/main/scala/com/v6ak/hagen/dashboards/Glance.scala
+++ src/main/scala/com/v6ak/hagen/dashboards/Glance.scala
@@ -1,5 +1,7 @@
 package com.v6ak.hagen.dashboards
 
+import com.v6ak.hagen.ifNonDefault
+import com.v6ak.hagen.dashboards.Glance.DefaultShowName
 import com.v6ak.hagen.expressions.{Const, Context, Entity, Expr}
 import com.v6ak.hagen.optionalMap
 
@@ -8,11 +10,21 @@ import com.v6ak.hagen.optionalMap
  */
 case class Glance(
   title: Option[String] = None,
+  showName: Boolean = DefaultShowName,
+  showIcon: Boolean = DefaultShowIcon,
+
 )(entities: (StatsEntity[_]|GlanceEntity[_])*) extends Card:
+  import Glance._
   override def toStructure(context: Context): Any = Map(
     "type" -> "glance",
     "entities" -> entities.map(_.toStructure(context)),
   ) ++
-    optionalMap("title", title)
+    optionalMap("title", title) ++
+    optionalMap("show_icon", ifNonDefault(showIcon, DefaultShowIcon)) ++
+    optionalMap("show_name", ifNonDefault(showName, DefaultShowName))
 
   override def variables: Set[Entity[_]] = entities.flatMap(_.variables).toSet
+
+object Glance:
+  val DefaultShowName = true
+  val DefaultShowIcon = true
