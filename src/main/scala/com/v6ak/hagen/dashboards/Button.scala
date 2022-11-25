e9f83752c2d810ed8eb81b2afa1e1048277af26f
Vít Šesták <v6ak@users.noreply.github.com>
2022-11-25 19:46:14 +0100

    Add showName to Button

--- src/main/scala/com/v6ak/hagen/dashboards/Button.scala
+++ src/main/scala/com/v6ak/hagen/dashboards/Button.scala
@@ -1,5 +1,5 @@
 package com.v6ak.hagen.dashboards
-import com.v6ak.hagen.dashboards.Button.DefaultShowIcon
+import com.v6ak.hagen.dashboards.Button.*
 import com.v6ak.hagen.{ifNonDefault, optionalMap}
 import com.v6ak.hagen.expressions.{Context, Entity}
 
@@ -11,6 +11,7 @@ case class Button(
   tapAction: Option[ButtonAction] = None,
   entity: Option[Entity[_]] = None,
   showIcon: Boolean = DefaultShowIcon,
+  showName: Boolean = DefaultShowName,
 ) extends Card:
   override def toStructure(context: Context): Any = (
     Map(
@@ -21,9 +22,11 @@ case class Button(
       optionalMap("icon_height", iconHeight) ++
       optionalMap("tap_action", tapAction.map(_.toStructure(context))) ++
       optionalMap("show_icon", ifNonDefault(showIcon, DefaultShowIcon)) ++
+      optionalMap("show_name", ifNonDefault(showName, DefaultShowName)) ++
       optionalMap("entity", entity.map(_.name))
   )
   override def variables: Set[Entity[_]] = tapAction.fold(Set())(_.variables) ++ entity
 
 object Button:
   val DefaultShowIcon = true
+  val DefaultShowName = true
