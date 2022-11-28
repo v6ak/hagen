46a004ad2e9bab75872439f24101253aa57c28ff
Vít Šesták <v6ak@users.noreply.github.com>
2022-11-28 19:49:08 +0100

    More options for EntityCard

--- src/main/scala/com/v6ak/hagen/dashboards/EntityCard.scala
+++ src/main/scala/com/v6ak/hagen/dashboards/EntityCard.scala
@@ -1,22 +1,36 @@
 package com.v6ak.hagen.dashboards
 
 import com.v6ak.hagen.optionalMap
-import com.v6ak.hagen.expressions.{Context, Entity}
+import com.v6ak.hagen.expressions.{Context, ContextDependentEntity, Entity, StateAttr}
 
 case class EntityCard(
-  entity: Entity[_],
+  entity: ContextDependentEntity[_],
   icon: Option[Icon] = None,
   attribute: Option[String] = None,
   name: Option[String] = None,
+  unit: Option[String] = None,
 ) extends Card:
   override def toStructure(context: Context): Any = (
     Map(
       "type" -> "entity",
-      "entity" -> entity.name,
+      "entity" -> entity.getName(context),
     ) ++
       optionalMap("icon", icon.map(_.toStructure)) ++
       optionalMap("attribute", attribute) ++
-      optionalMap("name", name)
+      optionalMap("name", name) ++
+      optionalMap("unit", unit)
   )
 
-  override def variables: Set[Entity[_]] = Set(entity)
+  override def variables: Set[Entity[_]] = entity.variables
+
+object EntityCard:
+  def forAttr(
+    attr: StateAttr[_],
+    icon: Option[Icon] = None,
+    name: Option[String] = None,
+  ): EntityCard = EntityCard(
+    entity = attr.entity,
+    attribute = Some(attr.name),
+    icon = icon,
+    name = name
+  )
