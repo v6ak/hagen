dc6c02d929c7126c6fd2dee54175133867d4d1dc
Vít Šesták <v6ak@users.noreply.github.com>
2022-11-28 15:15:56 +0100

    Add bounded

--- src/main/scala/com/v6ak/hagen/expressions/package.scala
+++ src/main/scala/com/v6ak/hagen/expressions/package.scala
@@ -3,5 +3,15 @@ package com.v6ak.hagen
 package object expressions {
   def max[T <: Int|Double](expressions: Expr[T]*): Expr[T] = unsafe.FuncCall("max", expressions: _*)
   def min[T <: Int|Double](expressions: Expr[T]*): Expr[T] = unsafe.FuncCall("min", expressions: _*)
+
+  def bounded[T <: Int | Double](value: Expr[T], lower: Expr[T], upper: Expr[T]): Expr[T] =
+    max(
+      lower,
+      min(
+        upper,
+        value
+      )
+    )
+
   def abs[T <: Int|Double](expression: Expr[T]): Expr[T] = unsafe.FilterExpr(expression, "abs")
 }
