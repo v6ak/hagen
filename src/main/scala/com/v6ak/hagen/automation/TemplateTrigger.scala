f92753d6cc75bbce8480312830cda322ff56c21e
Vít Šesták <v6ak@users.noreply.github.com>
2024-04-15 13:11:10 +0200

    TemplateTrigger: Implement duration for

--- src/main/scala/com/v6ak/hagen/automation/TemplateTrigger.scala
+++ src/main/scala/com/v6ak/hagen/automation/TemplateTrigger.scala
@@ -1,11 +1,20 @@
 package com.v6ak.hagen.automation
 
+import com.v6ak.hagen.{durationToMap, optionalMap}
 import com.v6ak.hagen.expressions.{Context, Entity, Expr}
 
-final case class TemplateTrigger(condition: Expr[Boolean]) extends Trigger[Nothing] :
+import scala.concurrent.duration.Duration
+
+final case class TemplateTrigger(
+  condition: Expr[Boolean],
+  duration: Option[Duration] = None,
+) extends Trigger[Nothing]:
   override def toStructure(context: Context): Any = Map(
     "platform" -> "template",
     "value_template" -> condition.toStructure(context),
   )
+    ++ optionalMap("for", duration.map(duration => durationToMap(duration)))
+
+  def duration(duration: Duration): TemplateTrigger = copy(duration = Some(duration))
 
   override def variables: Set[Entity[_]] = condition.variables
