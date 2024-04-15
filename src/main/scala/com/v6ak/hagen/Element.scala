9c3473b5e762e7e384df90e1e7baf6dda9f44cee
Vít Šesták <v6ak@users.noreply.github.com>
2024-04-15 13:28:40 +0200

    Element: code style

--- src/main/scala/com/v6ak/hagen/Element.scala
+++ src/main/scala/com/v6ak/hagen/Element.scala
@@ -4,8 +4,8 @@ import com.v6ak.hagen.expressions.{Context, Entity}
 
 trait Element:
   def toStructure(context: Context): Any
-  def variables: Set[Entity[_]]
-  def defined: Set[Entity[_]]
+  def variables: Set[Entity[?]]
+  def defined: Set[Entity[?]]
 
 trait NakeableElement extends Element:
   def toInnerStructure(context: Context): Any
@@ -14,29 +14,29 @@ trait TopLevelElement extends Element:
   override def toStructure(context: Context): Map[String, Element]
 
 trait TupleElement extends Element:
-  override def toStructure(context: Context): (String, Map[_, _])
+  override def toStructure(context: Context): (String, Map[?, ?])
 
 trait DelegatingElement extends Element:
   def subElements: Set[Element]
-  def variables: Set[Entity[_]] = subElements.flatMap(_.variables)
-  def defined: Set[Entity[_]] = subElements.flatMap(_.defined)
+  def variables: Set[Entity[?]] = subElements.flatMap(_.variables)
+  def defined: Set[Entity[?]] = subElements.flatMap(_.defined)
 
-final case class RawElement(structure: Any, variables: Set[Entity[_]], defined: Set[Entity[_]]) extends Element:
+final case class RawElement(structure: Any, variables: Set[Entity[?]], defined: Set[Entity[?]]) extends Element:
   override def toStructure(context: Context): Any = structure
 
 
 final case class MapTopLevelElement(structure: Map[String, Element]) extends TopLevelElement:
 
-  override def variables: Set[Entity[_]] = structure.values.flatMap(_.variables).toSet
+  override def variables: Set[Entity[?]] = structure.values.flatMap(_.variables).toSet
 
-  override def defined: Set[Entity[_]] = structure.values.flatMap(_.defined).toSet
+  override def defined: Set[Entity[?]] = structure.values.flatMap(_.defined).toSet
 
   override def toStructure(context: Context): Map[String, Element] = structure
 
 final case class NakedElement(inner: NakeableElement) extends Element{
   override def toStructure(context: Context): Any = inner.toInnerStructure(context)
 
-  override def variables: Set[Entity[_]] = inner.variables
+  override def variables: Set[Entity[?]] = inner.variables
 
-  override def defined: Set[Entity[_]] = inner.defined
+  override def defined: Set[Entity[?]] = inner.defined
 }
