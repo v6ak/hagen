a72b55fcbbacc1d49c441cd2377bb45b4a3c65d0
Vít Šesták <v6ak@users.noreply.github.com>
2024-04-15 11:48:22 +0200

    Grid: add title

--- src/main/scala/com/v6ak/hagen/dashboards/Grid.scala
+++ src/main/scala/com/v6ak/hagen/dashboards/Grid.scala
@@ -1,13 +1,18 @@
 package com.v6ak.hagen.dashboards
+import com.v6ak.hagen
 import com.v6ak.hagen.expressions.{Context, Entity}
 
-case class Grid(square: Boolean, columns: Int)(cards: Card*) extends Card:
+case class Grid(
+  square: Boolean,
+  columns: Int,
+  title: Option[String] = None,
+)(cards: Card*) extends Card:
   override def toStructure(context: Context): Any = Map(
     "type" -> "grid",
     "square" -> square,
     "columns" -> columns,
     "cards" -> cards.map(_.toStructure(context)),
-  )
+  ) ++ hagen.optionalMap("title", title)
 
   override def variables: Set[Entity[_]] = cards.flatMap(_.variables).toSet
 
