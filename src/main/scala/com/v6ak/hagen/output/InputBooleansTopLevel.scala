9926acc3f4ef24cbc63a49a46f46a0499b8639eb
Vít Šesták <v6ak@users.noreply.github.com>
2024-04-17 14:12:57 +0200

    Code style

--- src/main/scala/com/v6ak/hagen/output/InputBooleansTopLevel.scala
+++ src/main/scala/com/v6ak/hagen/output/InputBooleansTopLevel.scala
@@ -2,18 +2,16 @@ package com.v6ak.hagen.output
 
 import com.v6ak.HeteroMap
 
-object InputBooleansTopLevel extends HagenModule {
+object InputBooleansTopLevel extends HagenModule:
 
-  override def dependencies: Set[HagenKey[_]] = Set(InputBooleans)
+  override def dependencies: Set[HagenKey[?]] = Set(InputBooleans)
 
-  override def produces: Set[HagenKey[_]] = Set(TopLevelKeys)
+  override def produces: Set[HagenKey[?]] = Set(TopLevelKeys)
 
-  override def content(params: HeteroMap[HagenKey[_]]): HeteroMap[HagenKey[_]] = {
+  override def content(params: HeteroMap[HagenKey[?]]): HeteroMap[HagenKey[?]] = {
     HeteroMap(
       TopLevelKeys -> Map(
         "input_boolean" -> MapTuplesElement(params(InputBooleans))
       )
     )
   }
-
-}
